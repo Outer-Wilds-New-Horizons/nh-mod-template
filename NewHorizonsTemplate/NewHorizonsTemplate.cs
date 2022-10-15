05831d40e65b99eb3cc020390724f4f08a6be781
Noah Pilarski <mehmehmeh313@gmail.com>
2022-10-15 14:04:18 -0400

    Update template

--- NewHorizonsTemplate/NewHorizonsTemplate.cs
+++ NewHorizonsTemplate/NewHorizonsTemplate.cs
@@ -1,10 +1,13 @@
-﻿using OWML.ModHelper;
-using OWML.Common;
+﻿using OWML.Common;
+using OWML.ModHelper;
+using OWML.Utils;
 using UnityEngine;
 using UnityEngine.PostProcessing;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NewHorizonsTemplate
 {
@@ -14,7 +17,7 @@ namespace NewHorizonsTemplate
 
         private void Start()
         {
-            NewHorizonsAPI = ModHelper.Interaction.GetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizonsAPI = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
             NewHorizonsAPI.LoadConfigs(this);
         }
     }
